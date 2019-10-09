7e3a25654a74bf71b9a8de194aa2b81c240d660d
Parag <paragspatil123@gmail.com>
2019-10-09 16:04:45 +0530

    Quizapp complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:day_5_starter/quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 void main() {
   runApp(Quizlr());
 }
@@ -9,14 +10,10 @@ class Quizlr extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
           backgroundColor: Colors.grey.shade900,
-          body: SafeArea(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10.0),
-              child: QuizPage(),
-            ),
-          ),
+          body: QuizPage()
         ));
   }
 }
@@ -27,67 +24,151 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  Quizbrain brain = Quizbrain();
+
+  bool checkiflastquestion(){
+    return brain.QuetionNumber == brain.quetions.length-1;
+  }
+  int correctans = 0;
+  void checkAnswer(bool userAnswer) {
+
+    int wrongans = 0;
+    setState(() {
+      if (userAnswer == brain.quetions[brain.QuetionNumber].answer) {
+        scoreKeeper.add(Icon(
+          Icons.check,
+          color: Colors.green,
+
+
+        ));
+      } else {
+        scoreKeeper.add(Icon(
+          Icons.close,
+          color: Colors.red,
+        ));
+
+      }
+      if(checkiflastquestion()) {
+        Alert(
+          context: context,
+          type: AlertType.error,
+          title: "RFLUTTER ALERT",
+          desc: "Wr are all out of questions for you",
+          buttons: [
+            DialogButton(
+              child: Text(
+                "Reset",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () {
+                int i=0;
+                for(i=0;i<brain.quetions.length;i++) {
+                  scoreKeeper.removeLast();
+                }
+                brain.QuetionNumber = 0;
+              },
+              width: 120,
+            ),
+            DialogButton(
+              child: Text(
+                "Close",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () => Navigator.pop(context),
+              width: 120,
+            )
+          ],
+
+        ).show();
+
+      }
+      else {
+        brain.nextquetion();
+      }
+//      else{if(checkiflastquestion()){
+//
+//        }
+//      }
+    }
+      );
+    }
+
+ void restart(){
+    brain.QuetionNumber = 0;
+ }
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Expanded(
-          flex: 5,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                'This is where the question text will go',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 24.0,
-                  color: Colors.white,
+
+
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              flex: 5,
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Center(
+                  child: Text(
+                    brain.quetions[brain.QuetionNumber].quetion,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: FlatButton(
+                    textColor: Colors.white,
+                    color: Colors.green,
+                    child: Text(
+                      'True',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      checkAnswer(true);
+
+                    }),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: FlatButton(
+                  textColor: Colors.white,
+                  color: Colors.red,
+                  child: Text(
+                    'False',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {
+                    checkAnswer(false);
+                  },
                 ),
               ),
-              onPressed: () {},
             ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
-              onPressed: () {},
-            ),
-          ),
-        ),
-        // TODO a row here to keep the scores
-      ],
-    );
+            Row(
+              children: scoreKeeper,
+            )
+          ],
+        );
+
   }
 }
+//_onAlertButtonPressed(context) {
+
 
 
 // Sample Questions and Answers
--- lib/question.dart
+++ lib/question.dart
@@ -1,5 +1,8 @@
 // Define Question class here
-
-
+class Question {
+  String quetion;
+  bool answer;
+  Question( this.quetion , this.answer);
+}
 
 
